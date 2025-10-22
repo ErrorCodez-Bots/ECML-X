415dc20448ee596f4a402539b9c7319aa6a31c27
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2025-10-22 10:02:55 +0530

    Update and rename wzml_minimal.py to ecml_minimal.py

--- bot/helper/themes/ecml_minimal.py
+++ bot/helper/themes/ecml_minimal.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-class WZMLStyle:
+class ECMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = "Repo"
-    ST_BN1_URL = "https://www.github.com/weebzone/WZML-X"
+    ST_BN1_URL = "https://www.github.com/ErrorCodez-Bots/ECML-X"
     ST_BN2_NAME = "Updates"
-    ST_BN2_URL = "https://t.me/WZML_X"
+    ST_BN2_URL = "https://t.me/ErrorCodez_Bots"
     ST_MSG = """<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>"""
     ST_BOTPM = """<i>Now, This bot will send all your files and links here. Start Using ...</i>"""
-    ST_UNAUTH = """<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>"""
+    ST_UNAUTH = """<i>You Are not authorized user! Deploy your own ECML-X Mirror-Leech bot</i>"""
     OWN_TOKEN_GENERATE = (
         """<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</i>"""
     )
@@ -23,7 +23,7 @@ class WZMLStyle:
 <b>Validity:</b> {validity}"""
     # ---------------------
     # async def token_callback(_, query): ---> __main__.py
-    ACTIVATED = "✅️ Activated ✅"
+    ACTIVATED = "Aᴄᴛɪᴠᴀᴛᴇᴅ ⚡"
     # ---------------------
     # async def login(_, message): --> __main__.py
     LOGGED_IN = "<b>Already Bot Login In!</b>"
